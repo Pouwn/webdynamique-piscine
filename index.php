4ba060ef221e73686d30e0e6f880db795a50abef
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2022-05-29 02:39:37 +0200

    update home page

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Page d'Accueil </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header_footer/header_footer.css">
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+
+<div style="background-image: url('images/headerimg.jpg'); background-size: cover; width:100%; height:450px;"  class="header">
+    <div class="fff">
+
+        <img class="faq" src="images\icon\faq.png" width="60px" height="auto" alt="">
+
+    </div>
+
+    <div class="cont">
+
+        <div class="liens">
+            <button class="button">Accueil</button>
+
+            <div class="milieu">
+                <button class="button">Tout parcourir</button>
+                <img src="images\logonoir.png" width="180" class="m-3" height="180" style="border-radius: 50%;" alt="">
+                <button class="button"> RDV</button>
+            </div>
+
+            <button class="button"> Mon compte</button>
+        </div>
+
+    </div>
+
+</div>
+
+<!-- fin du header -->
+
+<img src="images/equipe" alt="" width="400" height="auto" class="mx-auto d-block equipe">
+
+<h2 class="text-primary m-5 text-center" >Nos spécilalités</h2>
+
+<div class="medecins">
+
+    <img src="images\med1.webp" alt="">
+    <img src="images\med2.png" alt="">
+    <img src="images\med3.jpeg" alt="">
+    <img src="images\med4.png" alt="">
+</div>
+
+<div class="span">
+    <button class="bg-primary text-white p-2"> Bulletin santé de la semaine </button>
+</div>
+
+<h5 class="text-center">Ce samedi 28-05-22 nous vous accueillons pour parler de <br> la
+    <span class="text-primary">santé chez l'enfant </span>
+    , inscrivez vous et à très bientot.</h5>
+
+
+<div class="m-5">
+    <img src="images\visuel-sante-du-futur-web-actu.jpeg" width="40%" height="auto" class="mx-auto d-block" alt="">
+</div>
+
+<div class="span">
+    <button class="bg-primary text-white p-2"> Nos principales <br> valeures </button>
+</div>
+
+<div class="troisimg">
+    <img src="images\footer1.jpeg" alt="">
+    <img src="images\footer2.png" alt="">
+    <img src="images\footer3.png" alt="">
+</div>
+
+<!-- début du footer-->
+<div class="container-fluid" id="footer">
+    <div class="row">
+        <div class="col">position<br>
+            <!-- lien pour la carte google map-->
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.351678858319!2d2.285043715830529!3d48.
+            851503909145414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6700497ee3ec5%3A0xdd60f514adcdb346!2s37%20Quai%20de%20Grenelle%2C%2075015%20Paris!
+            5e0!3m2!1sfr!2sfr!4v1653397775905!5m2!1sfr!2sfr"
+                    width="auto" height="auto" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="col" id="midlefooter">email</div>
+
+        <div class="col">contact</div>
+
+    </div>
+</div>
+<!-- fin du footer-->
+
+</body>
+</html>
